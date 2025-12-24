ebf4e1f9a2eac98208d92059c101c8c8f8448689
Mann Prashantkumar Bhavsar <2203051051102@paruluniversity.ac.in>
2025-12-24 19:40:51 +0530

    Update DATA CLEANING2.sql

--- DATA CLEANING2.sql
+++ DATA CLEANING2.sql
@@ -1,16 +1,4 @@
--- data cleaning2 
-
--- now when we are data cleaning we usually follow a few steps
--- 1. check for duplicates and remove any
--- 2. standardize data and fix errors
--- 3. Look at null values and see what 
--- 4. remove any columns and rows that are not necessary - few ways
-
-
-
--- 1. Remove Duplicates
-
-# First let's check for duplicates
+--1. Remove Duplicates
 
 
 SELECT *
